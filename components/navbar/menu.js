84d4eb362d26f2992b91ecf4275176ec0d3a901f
Marcin <kul.marcin@gmail.com>
2022-03-25 01:12:31 +0100

    navbar changed from router to state

--- components/navbar/menu.js
+++ components/navbar/menu.js
@@ -2,12 +2,26 @@ import * as React from 'react';
 import NavLink from '../utils/activeLink';
 
 export default function BasicMenu() {
+  const [current, setCurrent] = React.useState('/');
+  const clickHandler = e => {
+    e.preventDefault();
+    setCurrent(e.target.attributes[0].value);
+  };
+
   return (
     <div>
-      <NavLink href="/">About</NavLink>
-      <NavLink href="#services">Services</NavLink>
-      <NavLink href="#pricing">Pricing</NavLink>
-      <NavLink href="#contact">Contact</NavLink>
+      <NavLink click={clickHandler} href="/" current={current}>
+        About
+      </NavLink>
+      <NavLink click={clickHandler} href="#services" current={current}>
+        Services
+      </NavLink>
+      <NavLink click={clickHandler} href="#pricing" current={current}>
+        Pricing
+      </NavLink>
+      <NavLink click={clickHandler} href="#contact" current={current}>
+        Contact
+      </NavLink>
     </div>
   );
 }
--- components/utils/activeLink.js
+++ components/utils/activeLink.js
@@ -1,27 +1,16 @@
-import { useRouter } from 'next/router';
+import * as React from 'react';
 import styles from '../../styles/menu.module.css';
 
-function ActiveLink({ children, href }) {
-  const router = useRouter();
-
-  const handleClick = e => {
-    e.preventDefault();
-    router.push(href);
-  };
-
+function ActiveLink({ children, href, current, click }) {
   return (
     <a
       href={href}
-      onClick={handleClick}
-      className={`${styles.link} ${
-        router.asPath == `${href !== '/' ? '/' : ''}${href}`
-          ? styles.linkActive
-          : ''
-      }`}
+      onClick={click}
+      className={`${styles.link} ${current == href ? styles.linkActive : ''}`}
     >
       {children}
     </a>
-  )
+  );
 }
 
 export default ActiveLink;
