e5bb98abfb3460c2928649f8ee21c921693e6e5b
Marcin <roozeek@icloud.com>
2022-03-26 20:52:01 +0100

    added calculator template

--- components/pricing/Pricing.js
+++ components/pricing/Pricing.js
@@ -1,5 +1,6 @@
 import styles from '../../styles/Pricing.module.css';
 import { Typography } from '@mui/material';
+import Calculator from './calculator/Calculator';
 
 export default function Pricing() {
   return (
@@ -8,9 +9,10 @@ export default function Pricing() {
         Pricing
       </Typography>
       <Typography variant="h6" component="h6" sx={{marginTop: 3, marginBottom: 2}}>
-        Check estimated price of your product
+        Check estimated price of your project
       </Typography>
       <Typography variant="body1">Select items below which best describes your app and the features you require</Typography>
+      <Calculator/>
     </div>
   );
 }
--- components/pricing/calculator/Calculator.js
+++ components/pricing/calculator/Calculator.js
@@ -1,9 +1,101 @@
-import styles from '../../../styles/Calculator.module.css'
+import styles from '../../../styles/Calculator.module.css';
+
+import ComputerIcon from '@mui/icons-material/Computer';
+import SmartphoneIcon from '@mui/icons-material/Smartphone';
+import CloudIcon from '@mui/icons-material/Cloud';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
 
 export default function Calculator() {
-    return (
-        <div className={styles.calculator}>
-            hello
-        </div>
-    )
+  return (
+    <div className={styles.calculator}>
+      <Accordion
+        sx={{ width: 350, backgroundColor: 'lightgray', textShadow: 'none' }}
+      >
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography sx={{fontWeight: 'bold'}}>Size</Typography>
+        </AccordionSummary>
+        <AccordionDetails></AccordionDetails>
+      </Accordion>
+
+      <Accordion
+        sx={{ width: 350, backgroundColor: 'lightgray', textShadow: 'none' }}
+      >
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography sx={{fontWeight: 'bold'}}>UI Refinement</Typography>
+        </AccordionSummary>
+        <AccordionDetails></AccordionDetails>
+      </Accordion>
+
+      <Accordion
+        sx={{ width: 350, backgroundColor: 'lightgray', textShadow: 'none' }}
+      >
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography sx={{fontWeight: 'bold'}}>Users or Accounts</Typography>
+        </AccordionSummary>
+        <AccordionDetails></AccordionDetails>
+      </Accordion>
+
+      <Accordion
+        sx={{ width: 350, backgroundColor: 'lightgray', textShadow: 'none' }}
+      >
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography sx={{fontWeight: 'bold'}}>User Generated Content</Typography>
+        </AccordionSummary>
+        <AccordionDetails></AccordionDetails>
+      </Accordion>
+
+      <Accordion
+        sx={{ width: 350, backgroundColor: 'lightgray', textShadow: 'none' }}
+      >
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography sx={{fontWeight: 'bold'}}>Social</Typography>
+        </AccordionSummary>
+        <AccordionDetails></AccordionDetails>
+      </Accordion>
+
+      <Accordion
+        sx={{ width: 350, backgroundColor: 'lightgray', textShadow: 'none' }}
+      >
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography sx={{fontWeight: 'bold'}}>eCommerce</Typography>
+        </AccordionSummary>
+        <AccordionDetails></AccordionDetails>
+      </Accordion>
+
+      <Accordion
+        sx={{ width: 350, backgroundColor: 'lightgray', textShadow: 'none' }}
+      >
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography sx={{fontWeight: 'bold'}}>Management</Typography>
+        </AccordionSummary>
+        <AccordionDetails></AccordionDetails>
+      </Accordion>
+
+      <Accordion
+        sx={{ width: 350, backgroundColor: 'lightgray', textShadow: 'none' }}
+      >
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography sx={{fontWeight: 'bold'}}>Mobile</Typography>
+        </AccordionSummary>
+        <AccordionDetails></AccordionDetails>
+      </Accordion>
+
+      <Accordion
+        sx={{ width: 350, backgroundColor: 'lightgray', textShadow: 'none' }}
+      >
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography sx={{fontWeight: 'bold'}}>Cloud Solutions</Typography>
+        </AccordionSummary>
+        <AccordionDetails></AccordionDetails>
+      </Accordion>
+
+
+        <div className={styles.estimation}>Total Cost: </div>
+    </div>
+  );
 }
