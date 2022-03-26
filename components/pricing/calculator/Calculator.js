00fe1c3af03b1f8078a286b254260b44e454f778
Marcin <roozeek@icloud.com>
2022-03-26 21:39:42 +0100

    calculator template update

--- components/pricing/calculator/Calculator.js
+++ components/pricing/calculator/Calculator.js
@@ -1,8 +1,5 @@
 import styles from '../../../styles/Calculator.module.css';
 
-import ComputerIcon from '@mui/icons-material/Computer';
-import SmartphoneIcon from '@mui/icons-material/Smartphone';
-import CloudIcon from '@mui/icons-material/Cloud';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 import Accordion from '@mui/material/Accordion';
@@ -10,92 +7,159 @@ import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
 
+import Chip from '@mui/material/Chip';
+import Stack from '@mui/material/Stack';
+
 export default function Calculator() {
+  const handleClick = () => {
+    console.log('hey');
+  };
   return (
     <div className={styles.calculator}>
       <Accordion
         sx={{ width: 350, backgroundColor: 'lightgray', textShadow: 'none' }}
       >
         <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-          <Typography sx={{fontWeight: 'bold'}}>Size</Typography>
+          <Typography sx={{ fontWeight: 'bold' }}>Size</Typography>
         </AccordionSummary>
-        <AccordionDetails></AccordionDetails>
+        <AccordionDetails>
+          <Stack
+            direction="row"
+            spacing={1}
+            sx={{ flexWrap: 'wrap', justifyContent: 'center' }}
+          >
+            <Chip label="Small" variant="" onClick={handleClick} />
+            <Chip label="Medium" variant="outlined" onClick={handleClick} />
+            <Chip label="Large" variant="outlined" onClick={handleClick} />
+          </Stack>
+        </AccordionDetails>
       </Accordion>
 
       <Accordion
         sx={{ width: 350, backgroundColor: 'lightgray', textShadow: 'none' }}
       >
         <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-          <Typography sx={{fontWeight: 'bold'}}>UI Refinement</Typography>
+          <Typography sx={{ fontWeight: 'bold' }}>UI Refinement</Typography>
         </AccordionSummary>
-        <AccordionDetails></AccordionDetails>
+        <AccordionDetails>
+        <Stack
+            direction="row"
+            spacing={1}
+            sx={{ flexWrap: 'wrap', justifyContent: 'center' }}
+          >
+            <Chip label="Minimum" variant="" onClick={handleClick} />
+            <Chip label="Basic" variant="outlined" onClick={handleClick} />
+            <Chip label="Polished" variant="outlined" onClick={handleClick} />
+          </Stack>
+
+        </AccordionDetails>
       </Accordion>
 
       <Accordion
         sx={{ width: 350, backgroundColor: 'lightgray', textShadow: 'none' }}
       >
         <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-          <Typography sx={{fontWeight: 'bold'}}>Users or Accounts</Typography>
+          <Typography sx={{ fontWeight: 'bold' }}>Users</Typography>
         </AccordionSummary>
-        <AccordionDetails></AccordionDetails>
+        <AccordionDetails>
+
+        <Stack
+            direction="row"
+            spacing={1}
+            sx={{ flexWrap: 'wrap', justifyContent: 'center' }}
+          >
+            <Chip label="Classic Signup" variant="" onClick={handleClick} sx={{marginBottom: 1}} />
+            <Chip label="OAuth Signup" variant="outlined" onClick={handleClick} />
+            <Chip label="Multi-tenant Accounts" variant="outlined" onClick={handleClick}/>
+          </Stack>
+        </AccordionDetails>
       </Accordion>
 
       <Accordion
         sx={{ width: 350, backgroundColor: 'lightgray', textShadow: 'none' }}
       >
         <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-          <Typography sx={{fontWeight: 'bold'}}>User Generated Content</Typography>
+          <Typography sx={{ fontWeight: 'bold' }}>
+            User Generated Content
+          </Typography>
         </AccordionSummary>
-        <AccordionDetails></AccordionDetails>
+        <AccordionDetails>
+
+        <Stack
+            direction="row"
+            spacing={1}
+            sx={{ flexWrap: 'wrap', justifyContent: 'center' }}
+          >
+            <Chip label="Dashboard" variant="" onClick={handleClick} sx={{marginBottom: 1}} />
+            <Chip label="File Uploading" variant="outlined" onClick={handleClick} />
+            <Chip label="Profiles" variant="outlined" onClick={handleClick} />
+            <Chip label="Emails" variant="outlined" onClick={handleClick} />
+            <Chip label="Ratings" variant="outlined" onClick={handleClick} />
+          </Stack>
+        </AccordionDetails>
       </Accordion>
 
       <Accordion
         sx={{ width: 350, backgroundColor: 'lightgray', textShadow: 'none' }}
       >
         <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-          <Typography sx={{fontWeight: 'bold'}}>Social</Typography>
+          <Typography sx={{ fontWeight: 'bold' }}>Social</Typography>
         </AccordionSummary>
-        <AccordionDetails></AccordionDetails>
+        <AccordionDetails>
+
+        <Stack
+            direction="row"
+            spacing={1}
+            sx={{ flexWrap: 'wrap', justifyContent: 'center' }}
+          >
+            <Chip label="Messaging" variant="" onClick={handleClick} />
+            <Chip label="Forums" variant="outlined" onClick={handleClick} />
+            <Chip label="Social Sharing" variant="outlined" onClick={handleClick} />
+          </Stack>
+        </AccordionDetails>
       </Accordion>
 
       <Accordion
         sx={{ width: 350, backgroundColor: 'lightgray', textShadow: 'none' }}
       >
         <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-          <Typography sx={{fontWeight: 'bold'}}>eCommerce</Typography>
+          <Typography sx={{ fontWeight: 'bold' }}>eCommerce</Typography>
         </AccordionSummary>
-        <AccordionDetails></AccordionDetails>
+        <AccordionDetails>
+
+        <Stack
+            direction="row"
+            spacing={1}
+            sx={{ flexWrap: 'wrap', justifyContent: 'center' }}
+          >
+            <Chip label="Subscription" variant="" onClick={handleClick} sx={{marginBottom: 1}} />
+            <Chip label="Shopping Cart" variant="outlined" onClick={handleClick} />
+            <Chip label="Product Management" variant="outlined" onClick={handleClick} />
+          </Stack>
+        </AccordionDetails>
       </Accordion>
 
       <Accordion
         sx={{ width: 350, backgroundColor: 'lightgray', textShadow: 'none' }}
       >
         <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-          <Typography sx={{fontWeight: 'bold'}}>Management</Typography>
+          <Typography sx={{ fontWeight: 'bold' }}>Management</Typography>
         </AccordionSummary>
-        <AccordionDetails></AccordionDetails>
+        <AccordionDetails>
+
+        <Stack
+            direction="row"
+            spacing={1}
+            sx={{ flexWrap: 'wrap', justifyContent: 'center' }}
+          >
+            <Chip label="CMS Integration" variant="" onClick={handleClick} sx={{marginBottom:1}} />
+            <Chip label="Analytics" variant="outlined" onClick={handleClick} />
+            <Chip label="Multilingual Support" variant="outlined" onClick={handleClick} />
+          </Stack>
+        </AccordionDetails>
       </Accordion>
 
-      <Accordion
-        sx={{ width: 350, backgroundColor: 'lightgray', textShadow: 'none' }}
-      >
-        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-          <Typography sx={{fontWeight: 'bold'}}>Mobile</Typography>
-        </AccordionSummary>
-        <AccordionDetails></AccordionDetails>
-      </Accordion>
-
-      <Accordion
-        sx={{ width: 350, backgroundColor: 'lightgray', textShadow: 'none' }}
-      >
-        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-          <Typography sx={{fontWeight: 'bold'}}>Cloud Solutions</Typography>
-        </AccordionSummary>
-        <AccordionDetails></AccordionDetails>
-      </Accordion>
-
-
-        <div className={styles.estimation}>Total Cost: </div>
+      <div className={styles.estimation}>Total Cost: $0</div>
     </div>
   );
 }
